5bd7a2ee7fadbf6bb1107659937b593e616587b6
MatusDurana <matusdurana2@gmail.com>
2019-11-14 09:16:06 +0100

    Done

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int result = str.Length - str.Replace(letter.ToString(), "").Length;
+            return result;
+
         }
     }
 }
